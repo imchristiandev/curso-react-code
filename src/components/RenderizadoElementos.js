06e9d22fa3db90a5dc00723bb716180ea62cbd17
imchristiandev <imchristiandev@gmail.com>
2022-07-03 16:53:33 -0500

    feat: avance de código unidad 8 y 9

---
+++ src/components/RenderizadoElementos.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import data from "../helpers/data.json"
+
+function ElementoLista (props) {
+    return (
+    <li>
+        <a href={props.el.web} rel="noreferrer" target="_blank">{props.el.name}</a>
+    </li>
+    )
+}
+
+export default class RenderizadoElementos extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            seasons: ["Primavera", "Verano", "Otoño", "Invierno"]
+        };
+    }
+    render() {
+        return (
+            <div>
+                <h2>Renderizado de elementos</h2>  
+                <h3>Estaciones del año</h3>
+                <ol>
+                {
+                    this.state.seasons.map((season, index) => <li key={season}>{season}</li>)
+                }
+                </ol>
+                <h3>Frameworks Frontend JavaScript</h3>
+                <ul>
+                {
+                    data.frameworks.map((el) => <ElementoLista key={el.id} el={el}/>)
+                }
+                </ul>
+            </div>
+        )
+    }
+}
